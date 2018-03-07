c65f6dbe8ffd4c3b402b648c55b8ee1e6de051c9
Ahmed Magdy <ahmedmgd715@gmail.com>
2018-03-07 13:48:19 +0200

    'test'ing'

--- src/settings.py
+++ src/settings.py
@@ -121,8 +121,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 LOGIN_URL = 'accounts:login'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'live-static', 'static-root')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'live-static', 'media-root')
