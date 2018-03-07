f006c096a82ffd62071b1c1417a439210df28131
Ahmed Magdy <ahmedmgd715@gmail.com>
2018-03-07 15:31:02 +0200

    test #3

--- src/settings.py
+++ src/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'src.wsgi.application'
     # }
 # }
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
